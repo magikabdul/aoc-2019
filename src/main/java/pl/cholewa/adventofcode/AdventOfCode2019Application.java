1417f7b42c7036881381087649fbcc50716adc2e
Krzysztof Cholewa (Trecom) <krzysztof.cholewa@trecom.pl>
2019-12-03 13:47:37 +0100

    chore: inital commit

---
+++ src/main/java/pl/cholewa/adventofcode/AdventOfCode2019Application.java
@@ -0,0 +1,13 @@
+package pl.cholewa.adventofcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventOfCode2019Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventOfCode2019Application.class, args);
+    }
+
+}
